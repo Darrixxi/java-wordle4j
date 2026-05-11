f268b683750ba20dcb7dc4aef670ca0442433b26
Даша Белова <Dashabelova003@yandex.ru>
2026-05-11 23:04:50 +0300

    удален неиспользуемый импорт

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.nio.charset.StandardCharsets;
-import java.util.zip.DataFormatException;
 
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
