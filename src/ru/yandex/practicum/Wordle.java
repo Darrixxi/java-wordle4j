2d8d8fdead6cee9c831925420c5eac553f175ce5
Даша Белова <Dashabelova003@yandex.ru>
2026-05-11 22:57:58 +0300

    реализована логика игры, добавлены тесты

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,5 +1,11 @@
 package ru.yandex.practicum;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
 в главном классе нам нужно:
     создать лог-файл (он должен передаваться во все классы)
@@ -11,8 +17,44 @@ package ru.yandex.practicum;
  */
 public class Wordle {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (PrintWriter log = new PrintWriter(new FileWriter("game.log"), true)) {
+            WordleDictionaryLoader loader = new WordleDictionaryLoader(log);
+            ArrayList<String> rawWords = loader.load("words_ru.txt");
 
+            WordleDictionary dictionary = new WordleDictionary(rawWords);
+            WordleGame game = new WordleGame(dictionary, log);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Игра началась! Введите слово из 5 букв");
+
+            while (!game.isGameOver()) {
+                System.out.println("Осталось попыток: " + game.getSteps());
+                System.out.print("Введите слово: ");
+
+                String input = scanner.nextLine();
+
+                if (input.isBlank()) {
+                    System.out.println("Подсказка: " + game.suggestWord());
+                    continue;
+                }
+
+                try {
+                    String feedback = game.makeMove(input);
+                    System.out.println(feedback);
+                } catch (InvalidWordException e) {
+                    System.out.println("Ошибка: " + e.getMessage());
+                }
+            }
+            if (game.isWin()) {
+                System.out.println("Поздравляю! Вы угадали слово!");
+            } else {
+                System.out.println("Попыток больше нет. Вы проиграли. Загаданное слово было: " + game.getAnswer());
+            }
+
+            scanner.close();
+        } catch (IOException | DictionaryLoadException e) {
+            System.err.println("Критическая ошибка: " + e.getMessage());
+        }
     }
-
 }
--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Random;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -11,4 +15,30 @@ public class WordleDictionary {
 
     private List<String> words;
 
+    private final Random random = new Random();
+
+    public WordleDictionary(List<String> rawWords) {
+        ArrayList<String> cleanWords = new ArrayList<>();
+        for (String word : rawWords) {
+            String normalizedWord = word.toLowerCase().replace("ё", "е").trim();
+            if (normalizedWord.length() == 5) {
+                cleanWords.add(normalizedWord);
+            }
+        }
+        this.words = cleanWords;
+    }
+
+    public String getRandomWord() {
+        int randomWord = random.nextInt(words.size());
+        return words.get(randomWord);
+    }
+
+    public boolean contains(String word) {
+        String normalized = word.toLowerCase().replace("ё", "e").trim();
+        return words.contains(normalized);
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
 }
